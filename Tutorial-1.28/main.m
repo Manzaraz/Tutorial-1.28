cdda7c377b2027fa7fd08e8792c2d0168afd54a1
Christian Manzaraz <magnusmanz@gmail.com>
2024-10-15 21:29:04 -0300

    agregando un coche mas con structs en objective-c

--- Tutorial-1.28/main.m
+++ Tutorial-1.28/main.m
@@ -21,17 +21,24 @@ typedef struct automovil TipoCoche;
 int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
-    
+        
         TipoCoche coche;
+        TipoCoche coche2;
         
         coche.precio=4000;
         coche.kilometros=60000.40;
-        
         strcpy(coche.modelo, "Opel Corsa");
-        
         NSLog(@"Mi coche cuesta €%i", coche.precio);
         NSLog(@"Mi coche tiene %fKm", coche.kilometros);
         NSLog(@"Mi coche es un %s", coche.modelo);
+        
+        coche2.precio=1200;
+        coche2.kilometros=200;
+        strcpy(coche2.modelo, "Toyota Avensis");
+        NSLog(@"Mi coche cuesta €%i", coche2.precio);
+        NSLog(@"Mi coche tiene %fKm", coche2.kilometros);
+        NSLog(@"Mi coche es un %s", coche2.modelo);
+        
         
         
     }
