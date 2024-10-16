8c0a74afe70b4b22872cf0ada97a00af8457e5cd
Christian Manzaraz <magnusmanz@gmail.com>
2024-10-15 21:08:50 -0300

    Initial Commit

---
+++ Tutorial-1.28/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tutorial-1.28
+//
+//  Created by Christian Manzaraz on 15/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
